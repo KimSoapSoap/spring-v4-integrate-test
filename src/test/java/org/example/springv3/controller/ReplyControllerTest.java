98e9289ac968ce674e71ad54b37a968208a0f3e2
kimsoapsoap <zerfa@naver.com>
2024-10-24 01:21:46 +0900

    reply

--- src/test/java/org/example/springv3/controller/ReplyControllerTest.java
+++ src/test/java/org/example/springv3/controller/ReplyControllerTest.java
@@ -1,4 +1,12 @@
 package org.example.springv3.controller;
 
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class ReplyControllerTest {
 }
